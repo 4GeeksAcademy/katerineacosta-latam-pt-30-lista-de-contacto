aca73a8ff77d46fae569bcccd853a1f674a36059
kath424 <acostak424@gmail.com>
2024-06-08 08:07:20 +0000

    correcciones

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -20,7 +20,7 @@ export const Home = () => {
 					<a type="button" className="btn btn-success" href="/contact-add"> Add new contacto</a>
 				</div>
 				<div>
-					{ store.contacts.length > 0 && store.contacts.map(contact => (<div className="card">
+					{ store.contacts.length > 0 && store.contacts.map(contact => (<div key={contact.id} className="card">
 						<div className="row g-0">
 							<div className="col-md-2 ms-5 text-center">
 								<img src={userImagen} className="my-2 contact__img" alt="Profile picture"/>
@@ -50,20 +50,20 @@ export const Home = () => {
 		</div>
 	</div>
 
-	<div className="modal" tabindex="-1" id="deleteModal">
+	<div className="modal" id="deleteModal">
 		<div className="modal-dialog">
 			<div className="modal-content">
-			<div className="modal-header">
-				<h5 className="modal-title">Are you sure?</h5>
-				<button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-			</div>
-			<div className="modal-body">
-				<p>If you delete this thing the entire universe will go down!</p>
-			</div>
-			<div className="modal-footer">
-				<button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Oh no!</button>
-				<button type="button" onClick={deleteContact} className="btn btn-primary">Yes baby</button>
-			</div>
+				<div className="modal-header">
+					<h5 className="modal-title">Are you sure?</h5>
+					<button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+				</div>
+				<div className="modal-body">
+					<p>If you delete this thing the entire universe will go down!</p>
+				</div>
+				<div className="modal-footer">
+					<button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Oh no!</button>
+					<button type="button" className="btn btn-primary" data-bs-dismiss="modal" onClick={deleteContact} >Yes baby</button>
+				</div>
 			</div>
 		</div>
 	</div>
